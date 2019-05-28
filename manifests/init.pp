7364f65d9712d44bfccf715e92c5fcab76e6a033
Jesse Weisner <jesse_weisner@bcit.ca>
2019-05-27 18:40:31 -0700

    Initialized with PDK

---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include vaultbot
+class vaultbot (
+  $version,
+){
+  file { '/opt/vaultbot':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0555',
+  }
+
+  $base_url = "https://gitlab.com/msvechla/vaultbot/-/jobs/artifacts/v${version}/download?job=build"
+  $filename = "vaultbot-${version}.zip"
+
+  exec { "Download ${filename}":
+    cmd      => "curl -sL ${base_url} -o /opt/vaultbot/${filename}",
+    creates  => "/opt/vaultbot/${filename}",
+    notify   => Exec["Unpack ${filename}"],
+    requires => File['/opt/vaultbot'],
+  }
+
+  exec { "Unpack ${filename}":
+    cmd         => "unzip ${filename}",
+    refreshonly => true,
+    cwd         => '/opt/vaultbot',
+  }
+
+  file { '/etc/profile.d/vaultbot.sh':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0444',
+    source => 'modules/vaultbot/vaultbot.sh',
+  }
+}
